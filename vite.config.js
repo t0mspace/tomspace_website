8227b8cbc05764d7c602c40967797b721aca37c8
totoleheros <thomasbrenard@gmail.com>
2022-10-28 10:02:15 +0200

    add postcss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
 
 /** @type {import('vite').UserConfig} */
 export default {
-  plugins: [vue()],
+  plugins: [vue(), svgLoader()],
+  publicDir: 'public',
 }
