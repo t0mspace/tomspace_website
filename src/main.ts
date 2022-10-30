27294eba08040b1dd76410dc29d55e9a0ae59db1
totoleheros <thomasbrenard@gmail.com>
2022-10-30 20:24:03 +0100

    add full screen menu

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue/dist/vue.esm-bundler'
 import './index.scss'
 
-const app = createApp(App)
+const app = createApp({
+  data() {
+    return {
+      name: 'Thomas',
+      showMenu: false
+    }
+  },
+  methods: {
+    toggleNav: function () {
+      this.showMenu = !this.showMenu
+    },
+    scrollTo(refName: string) {
+      this.toggleNav()
+      const element = document.getElementById(refName)
+      const top = element.offsetTop
 
+      window.scrollTo({
+        top: top,
+        left: 0,
+        behavior: 'smooth'
+      })
+    }
+  }
+})
 app.mount('#app')
