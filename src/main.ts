a07679c26944cc2b7f8c31d6524ca3250e5f07b2
totoleheros <thomasbrenard@gmail.com>
2022-10-31 11:37:31 +0100

    add fixed header behavior

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,41 @@ import { createApp } from 'vue/dist/vue.esm-bundler'
 import './index.scss'
 
 const app = createApp({
-  data() {
-    return {
-      name: 'Thomas',
-      showMenu: false
-    }
-  },
-  methods: {
-    toggleNav: function () {
-      this.showMenu = !this.showMenu
+    data() {
+        return {
+            name: 'Thomas',
+            showMenu: false,
+        }
     },
-    scrollTo(refName: string) {
-      this.toggleNav()
-      const element = document.getElementById(refName)
-      const top = element.offsetTop
+    created() {
+        window.addEventListener('scroll', this.handleScroll)
+    },
+    destroyed() {
+        window.removeEventListener('scroll', this.handleScroll)
+    },
+    methods: {
+        toggleNav: function () {
+            this.showMenu = !this.showMenu
+        },
+        scrollTo(refName: string) {
+            this.toggleNav()
+            const element = document.getElementById(refName)
+            const top = element.offsetTop
 
-      window.scrollTo({
-        top: top,
-        left: 0,
-        behavior: 'smooth'
-      })
-    }
-  }
+            window.scrollTo({
+                top: top,
+                left: 0,
+                behavior: 'smooth',
+            })
+        },
+        handleScroll(event) {
+            const header = document.querySelector('.header')
+            if (window.scrollY >= window.innerHeight) {
+                header?.classList.add('header--isFixed')
+            } else {
+                header?.classList.remove('header--isFixed')
+            }
+        },
+    },
 })
 app.mount('#app')
